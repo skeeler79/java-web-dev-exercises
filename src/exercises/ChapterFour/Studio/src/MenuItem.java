0dddb968bf03f28dc4a1561cfd341019d4047256
Sabrina Keeler <92762914+skeeler79@users.noreply.github.com>
2022-07-25 20:15:20 -0400

    starting studio

---
+++ src/exercises/ChapterFour/Studio/src/MenuItem.java
@@ -0,0 +1,19 @@
+public class MenuItem {
+
+    private int price;
+    private String name;
+    private String description;
+    private int dateCreated;
+    private int dateUpdated;
+    private boolean isNew = true;
+
+    public MenuItem {
+        this.price = price;
+        this.name = name;
+        this.description = description;
+        this.dateCreated = dateCreated;
+        this.dateUpdated = dateUpdated;
+        this.isNew = isNew;
+
+    }
+}
