fdb74970933121f7b260d37ed2ef9ac7ba7d86fc
Sabrina Keeler <92762914+skeeler79@users.noreply.github.com>
2022-08-03 09:28:26 -0400

    exercises for chap 6 unit testing

--- src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
+++ src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
@@ -30,7 +30,25 @@ public class CarTest {
         assertFalse(test_car.getGasTankLevel() == 0);
     }
     //TODO: gasTankLevel is accurate after driving within tank range
+    @Test
+    public void testGasTankAfterDriving (){
+        test_car.drive(50);
+        assertEquals(9, test_car.getGasTankLevel(), .001);
+    }
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+
+    @Test
+    public void testGasTankAfterExceedingTankRange(){
+        test_car.drive(550);
+        assertEquals( 0, test_car.getGasTankLevel(),.001);
+    }
     //TODO: can't have more gas than tank size, expect an exception
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGasOverfillException() {
+    test_car.addGas(5);
+        fail("Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+
+    }
 
 }
